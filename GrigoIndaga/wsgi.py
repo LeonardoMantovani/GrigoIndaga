ea16e33753453d23f24d64c40099f3ffd2f384af
LeonardoMantovani <leonardo@mantovani.us>
2020-06-05 11:58:10 +0200

    Django Project Setup

---
+++ GrigoIndaga/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for GrigoIndaga project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GrigoIndaga.settings')
+
+application = get_wsgi_application()
---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    # Url per la homepage
+    # url(r'^$', views.homepage, name='homepage'),
+]
