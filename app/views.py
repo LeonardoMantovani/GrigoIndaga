62b3e9c6a35209b4ae79f823cc32c97dfbfb1f4c
LeonardoMantovani <leonardo@mantovani.us>
2020-06-06 14:42:59 +0200

    Mostrati nel dropdown di seleziona_classe.html le classi nel database (importabili da .csv tramite il pannello admin)

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Classe
 
 
 # View per la selezione della classe
 def seleziona_classe(request):
-    return render(request, 'seleziona_classe.html', {})
+    classi = Classe.objects.all()
+    return render(request, 'seleziona_classe.html', {'classi': classi})
