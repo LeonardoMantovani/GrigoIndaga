dfa987779a98e28757b18f270a3f21da3a574553
LeonardoMantovani <leonardo@mantovani.us>
2020-06-09 13:58:35 +0200

    Completato il meccanismo delle valutazioni (ora si vota tramite stelle da 1 a 10)

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.utils.safestring import mark_safe
 from .models import Classe, Votazione
+from django_starfield import Stars
 
 
 # Form per la scelta della classe
@@ -18,15 +20,48 @@ class FormSceltaClasse(forms.Form):
     pk_classe = forms.CharField(label="In che classe sei?", widget=forms.Select(choices=lista_classi))
 
 
+class ReadOnlyText(forms.TextInput):
+    input_type = 'text'
+
+    def render(self, name, value, attrs=None, renderer=None):
+        if value is None:
+            value = ''
+        return value
+
+
 # Form per votare un professore
 class FormVotaProfessore(forms.ModelForm):
-
-    # def clean_spiegazione(self):
-    #     spiegazione = self.cleaned_data['spiegazione']
-    #     if spiegazione < 1 or spiegazione > 10:
-    #         raise forms.ValidationError("Inserire un voto da 1 a 10")
-    #     return spiegazione
 
     class Meta:
         model = Votazione
         fields = ('spiegazione', 'preparazione', 'valutazioni', 'metodo', 'rapporto')
+        labels = {
+            'spiegazione': 'Chiarezza nelle Spiegazioni',
+            'preparazione': 'Preparazione',
+            'valutazioni': 'Adeguatezza delle Valutazioni',
+            'metodo': 'Efficacia del metodo',
+            'rapporto': 'Rapporto con gli Studenti',
+        }
+
+        # Crea una lista per le "choices" dei widgets RadioSelect composta 10 stelle
+        StelleDa1a10 = [
+            (1, mark_safe('<i class="fa fa-star" aria-hidden="true"></i>')),
+            (2, mark_safe('<i class="fa fa-star" aria-hidden="true"></i>')),
+            (3, mark_safe('<i class="fa fa-star" aria-hidden="true"></i>')),
+            (4, mark_safe('<i class="fa fa-star" aria-hidden="true"></i>')),
+            (5, mark_safe('<i class="fa fa-star" aria-hidden="true"></i>')),
+            (6, mark_safe('<i class="fa fa-star" aria-hidden="true"></i>')),
+            (7, mark_safe('<i class="fa fa-star" aria-hidden="true"></i>')),
+            (8, mark_safe('<i class="fa fa-star" aria-hidden="true"></i>')),
+            (9, mark_safe('<i class="fa fa-star" aria-hidden="true"></i>')),
+            (10, mark_safe('<i class="fa fa-star" aria-hidden="true"></i>'))
+        ]
+
+        # Imposta il widget di ogni parametro come un RadioSelect di 10 stelle
+        widgets = {
+            'spiegazione': forms.RadioSelect(choices=StelleDa1a10),
+            'preparazione': forms.RadioSelect(choices=StelleDa1a10),
+            'valutazioni': forms.RadioSelect(choices=StelleDa1a10),
+            'metodo': forms.RadioSelect(choices=StelleDa1a10),
+            'rapporto': forms.RadioSelect(choices=StelleDa1a10),
+        }
